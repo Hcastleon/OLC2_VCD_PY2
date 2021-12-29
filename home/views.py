74180c85f97f0f511142c25b1c49c3b83bcd3742
Heidy Castellanos <castellanos.heidyc@gmail.com>
2021-12-28 21:25:09 -0600

    Tendencia y arreglo de front

--- home/views.py
+++ home/views.py
@@ -43,4 +43,24 @@ def archivo(request):
     return render(request,'archivos.html',{'titulo':nombrecito, 'cabecera': encabezados, 'contenido' : diccionario})
 #funciones
 def funcion_1(request):
-    return render(request,'funciones/Tendencia_infeccion.html')
+    seleccionados = []
+    if request.method == 'POST':
+        logger = logging.getLogger('degub')
+        logger.info(request.POST)
+        pais_select = request.POST.get('camposelect','')
+        pais = request.POST.get('paisselect','')
+        tenden = request.POST.get('tendenciaselect','')
+        if pais_select != '' :
+            for i in diccionario:
+                for j in i:
+                    if j == pais_select:
+                        if not(i[j] in seleccionados):
+                            seleccionados.append(i[j])
+            return render(request,'funciones/Tendencia_infeccion.html',{'cabecera':encabezados, 'pais': seleccionados})
+        else:
+            if pais != '' and tenden != '' :
+                print(pais)
+                print(tenden)
+
+    else:
+        return render(request,'funciones/Tendencia_infeccion.html',{'cabecera':encabezados})
